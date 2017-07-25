982568712c8b3a48921b35b75d4ea89f004ca8b5
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-25 16:09:23 +0800

    Updated test.

--- portel-test.js
+++ portel-test.js
@@ -2,10 +2,10 @@
 const assert = require( "assert" );
 const portel = require( "./portel.js" );
 
-assert.equal( typeof portel( 12345 ), "object", "should have value 'object'" );
+assert.equal( typeof portel( 12345 ), "object", "should return 'object'" );
 
-assert.equal( typeof portel( "hello world" ), "object", "should have value 'object'" );
+assert.equal( typeof portel( "hello world" ), "object", "should return 'object'" );
 
-assert.equal( typeof portel( false ), "object", "should have value 'object'" );
+assert.equal( typeof portel( false ), "object", "should return 'object'" );
 
 console.log( "ok" );
