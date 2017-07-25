d0b65487d4ca06198f1068e06e27f8f4f2f1e1b8
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-25 16:10:05 +0800

    Updated dependency.

--- portel.support.js
+++ portel.support.js
@@ -34,10 +34,11 @@
               			"file": "portel.js",
               			"module": "portel",
               			"author": "Richeve S. Bebedor",
-              			"contributors": [
-              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-              			],
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon <vinsevinalon@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/portel.git",
               			"test": "portel-test.js",
               			"global": true
